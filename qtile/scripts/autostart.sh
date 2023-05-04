f9c382317a9e08a877d20321840ad36bdcee66ef
anrichtait <anrichjtait@gmail.com>
2023-05-04 16:24:19 +0200

    Added my qtile dots

---
+++ qtile/scripts/autostart.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep -x $(basename $1 | head -c 15) 1>/dev/null;
+  then
+    $@&
+  fi
+}
+#change your keyboard if you need it
+#setxkbmap -layout be
+
+keybLayout=$(setxkbmap -v | awk -F "+" '/symbols/ {print $2}')
+
+if [ $keybLayout = "be" ]; then
+  cp $HOME/.config/qtile/config-azerty.py $HOME/.config/qtile/config.py
+fi
+
+#Some ways to set your wallpaper besides variety or nitrogen
+feh --bg-fill /home/anricht/Pictures/pxfuel.jpg &
+feh --bg-fill /home/anricht/Pictures/pxfuel.jpg &
+
+#start sxhkd to replace Qtile native key-bindings
+run sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
+
+#starting utility applications at boot time
+#run variety &
+run nm-applet &
+run pamac-tray &
+run xfce4-power-manager &
+numlockx on &
+blueberry-tray &
+picom --config $HOME/.config/qtile/scripts/picom.conf &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/xfce4/notifyd/xfce4-notifyd &
+
+#starting user applications at boot time
+run volumeicon &
+
